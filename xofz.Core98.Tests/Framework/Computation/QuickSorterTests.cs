57e6f20fe42e3bdc8c198b87da0d5942a383cffb
Sam Pearson <sam@xofz.com>
2018-12-12 15:14:42 -0800

    Add QuickSorter

---
+++ xofz.Core98.Tests/Framework/Computation/QuickSorterTests.cs
@@ -0,0 +1,65 @@
+﻿namespace xofz.Tests.Framework.Computation
+{
+    using System;
+    using Ploeh.AutoFixture;
+    using xofz.Framework.Computation;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class QuickSorterTests
+    {
+        public class Context
+        {
+            public Context(ITestOutputHelper outputter)
+            {
+                this.outputter = outputter;
+                this.sorter = new QuickSorter();
+                this.fixture = new Fixture();
+            }
+
+            protected readonly ITestOutputHelper outputter;
+            protected readonly QuickSorter sorter;
+            protected readonly Fixture fixture;
+        }
+
+        public class When_Sort_is_called : Context
+        {
+            [Fact]
+            public void Sorts_the_input()
+            {
+                var f = this.fixture;
+                var o = this.outputter;
+                var testNumbers = new IComparable[0x1F];
+                for (var i = 0; i < testNumbers.Length; ++i)
+                {
+                    testNumbers[i] = f.Create<double>();
+                }
+
+                o.WriteLine("Unsorted:");
+                o.WriteLine(string.Join<IComparable>(
+                    " ", 
+                    testNumbers));
+                o.WriteLine(string.Empty);
+
+                this.sorter.Sort(testNumbers);
+
+                o.WriteLine("Sorted");
+                o.WriteLine(string.Join<IComparable>(
+                    " ",
+                    testNumbers));
+
+                for (var i = 0; i < testNumbers.Length - 1; ++i)
+                {
+                    Assert.True(testNumbers[i]
+                                    .CompareTo(testNumbers[i + 1]) <= 0);
+                }
+            }
+
+            public When_Sort_is_called(
+                ITestOutputHelper outputter) 
+                : base(outputter)
+            {
+            }
+        }
+    }
+}
---
+++ xofz.Core98/Framework/Computation/QuickSorter.cs
@@ -0,0 +1,121 @@
+﻿namespace xofz.Framework.Computation
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class QuickSorter
+    {
+        public virtual IComparable[] Sort(
+            IEnumerable<IComparable> finiteSource)
+        {
+            if (finiteSource is IComparable[] array)
+            {
+                this.Sort(array);
+                return array;
+            }
+
+            var sourceArray = EnumerableHelpers.ToArray(
+                finiteSource);
+            this.Sort(sourceArray, 0, sourceArray.Length - 1);
+
+            return sourceArray;
+        }
+
+        public virtual void Sort(
+            IComparable[] array)
+        {
+            if (array == null)
+            {
+                return;
+            }
+
+            this.Sort(array, 0, array.Length - 1);
+        }
+
+        public virtual void Sort(
+            IComparable[] array,
+            long left,
+            long right)
+        {
+            if (left < 0)
+            {
+                return;
+            }
+
+            if (right < 0)
+            {
+                return;
+            }
+
+            if (left >= right)
+            {
+                return;
+            }
+
+            if (left > array.Length - 1)
+            {
+                return;
+            }
+
+            if (right > array.Length - 1)
+            {
+                return;
+            }
+
+            ICollection<long> closedRights
+                = new LinkedList<long>();
+
+            beginSort:
+            long lowIndex = left, highIndex = right;
+            long pivotIndex;
+            checked
+            {
+                pivotIndex = left + right;
+            }
+
+            pivotIndex /= 2;
+            var pivot = array[pivotIndex];
+            while (lowIndex <= highIndex)
+            {
+                while (array[lowIndex].CompareTo(pivot) < 0)
+                {
+                    ++lowIndex;
+                }
+
+                while (array[highIndex].CompareTo(pivot) > 0)
+                {
+                    --highIndex;
+                }
+
+                if (lowIndex > highIndex)
+                {
+                    break;
+                }
+
+                var lowSwap = array[lowIndex];
+                array[lowIndex] = array[highIndex];
+                array[highIndex] = lowSwap;
+
+                ++lowIndex;
+                --highIndex;
+            }
+
+            if (left < highIndex)
+            {
+                closedRights.Add(right);
+                right = highIndex;
+                goto beginSort;
+            }
+
+            foreach (var closedRight in closedRights)
+            {
+                if (lowIndex < closedRight)
+                {
+                    left = lowIndex;
+                    right = closedRight;
+                    goto beginSort;
+                }
+            }
+        }
+    }
+}
