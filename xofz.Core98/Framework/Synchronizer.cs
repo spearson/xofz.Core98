59e9cd3ab9d51199d424ada4a70f452a6a2b6fb7
sam <sam@xofz.com>
2019-02-03 23:22:45 -0800

    Add Synchronizer

---
+++ xofz.Core98/Framework/Synchronizer.cs
@@ -0,0 +1,28 @@
+﻿namespace xofz.Framework
+{
+    public class Synchronizer<T>
+    {
+        public Synchronizer(T t)
+            : this(t, new object())
+        {
+        }
+
+        protected Synchronizer(T t, object locker)
+        {
+            this.t = t;
+            this.locker = locker;
+        }
+
+        public virtual void RunSync(Do<T> syncAction)
+        {
+            var actor = this.t;
+            lock (this.locker)
+            {
+                syncAction?.Invoke(actor);
+            }
+        }
+
+        protected readonly T t;
+        protected readonly object locker;
+    }
+}
