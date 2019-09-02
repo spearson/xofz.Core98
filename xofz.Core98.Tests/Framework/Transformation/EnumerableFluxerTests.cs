97b008ca5555a0aa893c2b95a079a449aa87c1d4
Sam Pearson <sam@xofz.com>
2019-09-02 13:55:08 -0700

    Add EnumerableFluxer and a test for it

---
+++ xofz.Core98.Tests/Framework/Transformation/EnumerableFluxerTests.cs
@@ -0,0 +1,58 @@
+﻿namespace xofz.Tests.Framework.Transformation
+{
+    using xofz.Framework.Transformation;
+    using Xunit;
+
+    public class EnumerableFluxerTests
+    {
+        public class Context
+        {
+            protected Context()
+            {
+                this.fluxer = new EnumerableFluxer();
+            }
+
+            protected readonly EnumerableFluxer fluxer;
+        }
+
+        public class When_Flux_is_called : Context
+        {
+            [Fact]
+            public void Fluxes_correctly()
+            {
+                var items = new long[]
+                {
+                    0x11,
+                    0x22,
+                    0x33,
+                    0x44,
+                    0x55,
+                    0x66,
+                    0x77,
+                    0x88,
+                    0x99
+                };
+
+                byte min = 1, max = 4;
+                var totalItemCount = 0;
+
+                foreach (var collection in this.fluxer.Flux(
+                    items,
+                    min,
+                    max))
+                {
+                    var c = collection.Count;
+                    totalItemCount += c;
+                    Assert.InRange(
+                        c,
+                        min,
+                        max);
+                }
+
+                Assert.Equal(
+                    totalItemCount,
+                    items.Length);
+            }
+        }
+    }
+}
---
+++ xofz.Core98/Framework/Transformation/EnumerableFluxer.cs
@@ -0,0 +1,78 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class EnumerableFluxer
+    {
+        public EnumerableFluxer()
+            : this(new Random())
+        {
+        }
+
+        protected EnumerableFluxer(
+            Random rng)
+        {
+            this.rng = rng;
+        }
+
+        public virtual IEnumerable<ICollection<T>> Flux<T>(
+            IEnumerable<T> source,
+            int min,
+            int max)
+        {
+            if (source == null)
+            {
+                yield break;
+            }
+
+            if (min < 1)
+            {
+                yield break;
+            }
+
+            if (min > max)
+            {
+                yield break;
+            }
+
+            var maxExclusive = max + 1;
+            if (max == int.MaxValue)
+            {
+                maxExclusive = max;
+            }
+
+            using (var e = source.GetEnumerator())
+            {
+                T nextCurrent = default;
+                byte indexer = 0;
+
+                addAndReturnChunk:
+                var nextChunkSize = this.rng.Next(min, maxExclusive);
+                var ll = new LinkedList<T>();
+                if (indexer > 0)
+                {
+                    ll.AddLast(nextCurrent);
+                }
+
+                while (indexer < nextChunkSize && e.MoveNext())
+                {
+                    ll.AddLast(e.Current);
+                    ++indexer;
+                }
+
+                yield return ll;
+                if (!e.MoveNext())
+                {
+                    yield break;
+                }
+
+                nextCurrent = e.Current;
+                indexer = 1;
+                goto addAndReturnChunk;
+            }
+        }
+
+        protected readonly Random rng;
+    }
+}
