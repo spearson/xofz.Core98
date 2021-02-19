46885b90d27ef1e310b2a4e820ae9f8c702e3106
Sam Pearson <sam@xofz.com>
2021-02-19 09:46:45 -0800

    Do some shit required by Microsoft to the build file; add Delayer

---
+++ xofz.Core98/Framework/Delayer.cs
@@ -0,0 +1,19 @@
+﻿namespace xofz.Framework
+{
+    public class Delayer
+    {
+        public virtual void Delay(
+            long milliseconds)
+        {
+            System.Threading.Thread.Sleep(
+                (int)milliseconds);
+        }
+
+        public virtual void Delay(
+            System.TimeSpan delay)
+        {
+            System.Threading.Thread.Sleep(
+                (int)delay.TotalMilliseconds);
+        }        
+    }
+}
