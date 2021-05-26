9a6779e34d00a44e314b2eee7cfd96058c105ae4
Sam Pearson <sam@xofz.com>
2021-05-25 20:41:14 -0700

    Add Raiser and RaiserTests

---
+++ xofz.Core98.Tests/Framework/Axioses/RaiserTests.cs
@@ -0,0 +1,67 @@
+﻿namespace xofz.Tests.Framework.Axioses
+{
+    using xofz.Framework;
+    using xofz.Framework.Axioses;
+    using Xunit;
+
+    public class RaiserTests
+    {
+        public class Context
+        {
+            protected Context()
+            {
+                this.raiser = new Raiser();
+                this.er = new EventRaiser();
+            }
+
+            protected readonly Raiser raiser;
+            protected readonly EventRaiser er;
+        }
+
+        public class When_Process_is_called 
+            : Context
+        {
+            [Fact]
+            public void Subscribes_behavior_to_Raised()
+            {
+                bool subscribed = false;
+                this.raiser.Process(
+                    () =>
+                    {
+                        subscribed = true;
+                    });
+
+                this.er.Raise(
+                    this.raiser,
+                    nameof(this.raiser.Raised));
+
+                Assert.True(
+                    subscribed);
+            }
+        }
+
+        public class When_Unprocess_is_called : Context
+        {
+            [Fact]
+            public void Unsubscribes_behavior_from_Raised()
+            {
+                bool unsubscribed = true;
+                Do behavior = () =>
+                {
+                    unsubscribed = false;
+                };
+                this.raiser.Process(
+                    behavior);
+                this.raiser.Unprocess(
+                    behavior);
+
+                this.er.Raise(
+                    this.raiser,
+                    nameof(this.raiser.Raised));
+
+                Assert.True(
+                    unsubscribed);
+            }
+        }
+    }
+}
---
+++ xofz.Core98/Framework/Axioses/Raiser.cs
@@ -0,0 +1,30 @@
+﻿namespace xofz.Framework.Axioses
+{
+    public class Raiser
+        : Axios
+    {
+        public virtual event Do Raised;
+
+        public virtual void Process(
+            Do behavior)
+        {
+            this
+                ?.GetType()
+                ?.GetEvent(nameof(this.Raised))
+                ?.AddEventHandler(
+                    this,
+                    behavior);
+        }
+
+        public virtual void Unprocess(
+            Do behavior)
+        {
+            this
+                ?.GetType()
+                ?.GetEvent(nameof(this.Raised))
+                ?.RemoveEventHandler(
+                    this,
+                    behavior);
+        }
+    }
+}
