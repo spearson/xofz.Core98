8a5c5ce6524ff3f358e709040bb4ec7ef8e4d407
Sam Pearson <xofz@outlook.com>
2022-01-18 20:16:45 -0800

    Make IndexedLinkedList an IList

--- xofz.Core98.Tests/IndexedLinkedListTests.cs
+++ xofz.Core98.Tests/IndexedLinkedListTests.cs
@@ -56,5 +56,57 @@
                     newObj1);
             }
         }
+
+        public class IList_test : Context
+        {
+            [Fact]
+            public void Go()
+            {
+                var newObj1 = new object();
+                var newObj2 = new object();
+                var newObj3 = new object();
+                this.ll.AddTail(newObj1);
+                this.ll.AddTail(newObj2);
+                this.ll.AddTail(newObj3);
+
+                Assert.Equal(
+                    1,
+                    this.ll.IndexOf(newObj2));
+
+                var newObj4 = new object();
+                this.ll[0] = newObj4;
+
+                Assert.Equal(
+                    newObj4,
+                    this.ll[0]);
+                Assert.Equal(
+                    0,
+                    this.ll.IndexOf(newObj4));
+
+                this.ll.Insert(1, newObj1);
+                Assert.Equal(
+                    1,
+                    this.ll.IndexOf(newObj1));
+
+                this.ll.RemoveAt(0);
+                Assert.Equal(
+                    0,
+                    this.ll.IndexOf(newObj1));
+
+                this.ll.AddTail(newObj4);
+                Assert.Equal(
+                    0,
+                    this.ll.IndexOf(newObj1));
+                Assert.Equal(
+                    1,
+                    this.ll.IndexOf(newObj2));
+                Assert.Equal(
+                    2,
+                    this.ll.IndexOf(newObj3));
+                Assert.Equal(
+                    3,
+                    this.ll.IndexOf(newObj4));
+            }
+        }
     }
 }
--- xofz.Core98/IndexedLinkedList.cs
+++ xofz.Core98/IndexedLinkedList.cs
@@ -3,13 +3,15 @@
     using System.Collections.Generic;
 
     public class IndexedLinkedList<T>
-        : XLinkedList<T>
+        : XLinkedList<T>, IList<T>
     {
         public static IndexedLinkedList<T> CreateIndexed(
             IEnumerable<T> finiteSource)
         {
             var ll = new IndexedLinkedList<T>();
-            populate(ll, finiteSource);
+            populate(
+                ll,
+                finiteSource);
 
             return ll;
         }
@@ -77,6 +79,57 @@
             }
 
             return result;
+        }
+
+        public virtual int IndexOf(
+            T item)
+        {
+            const short minusOne = -1;
+            int index = minusOne;
+            foreach (var node in this.readNodes(
+                         this.headNode,
+                         n => n.Next))
+            {
+                checked
+                {
+                    ++index;
+                }
+
+                if (node == null)
+                {
+                    break;
+                }
+
+                if (node.O?.Equals(item) ?? item == null)
+                {
+                    return index;
+                }
+            }
+
+            return minusOne;
+        }
+
+        public virtual void Insert(
+            int index,
+            T item)
+        {
+            this.AddBefore(
+                this.GetNode(index),
+                item);
+        }
+
+        public virtual void RemoveAt(
+            int index)
+        {
+            this.Remove(
+                this.GetNode(index));
+        }
+
+        public virtual T this[int index]
+        {
+            get => this[(long) index];
+
+            set => this[(long) index] = value;
         }
     }
 }
