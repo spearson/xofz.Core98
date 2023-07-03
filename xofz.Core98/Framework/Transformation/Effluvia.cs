7e8589895802a3dc2f798507ea76234f99945c01
Sam Pearson <xofz@outlook.com>
2023-07-02 20:39:13 -0700

    Add Effluvia

---
+++ xofz.Core98/Framework/Transformation/Effluvia.cs
@@ -0,0 +1,59 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+
+    public class Effluvia
+    {
+        public virtual IEnumerable<T> Generate<T>(
+            Gen<T> instantiate)
+        {
+            if (instantiate == null)
+            {
+                yield break;
+            }
+
+            while (truth)
+            {
+                yield return instantiate();
+            }
+        }
+
+        public virtual IEnumerable<T> Generate<T>(
+            Gen<T> instantiate,
+            Gen<T, bool> decider)
+        {
+            if (instantiate == null || decider == null)
+            {
+                yield break;
+            }
+
+            while (truth)
+            {
+                var nextItem = instantiate();
+                if (decider(nextItem))
+                {
+                    yield return nextItem;
+                    continue;
+                }
+
+                yield break;
+            }
+        }
+
+        public virtual IEnumerable<T> Generate<T>(
+            Gen<T> instantiate,
+            Gen<T, bool> decider,
+            Do<T> prepare)
+        {
+            foreach (var item in this.Generate(
+                         instantiate, 
+                         decider))
+            {
+                prepare?.Invoke(item);
+                yield return item;
+            }
+        }
+
+        protected const bool truth = true;
+    }
+}
