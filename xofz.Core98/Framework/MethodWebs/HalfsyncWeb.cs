c076bc164244794998dad6ddffe073c62c66fe6d
Sam Pearson <xofz@outlook.com>
2023-09-20 21:01:06 -0700

    Add HalfsyncWeb

---
+++ xofz.Core98/Framework/MethodWebs/HalfsyncWeb.cs
@@ -0,0 +1,59 @@
+﻿namespace xofz.Framework.MethodWebs
+{
+    using System.Collections.Generic;
+
+    public class HalfsyncWeb : HalflockWeb
+    {
+        public HalfsyncWeb()
+        {
+        }
+
+        protected HalfsyncWeb(
+            ICollection<Dependency> dependencies)
+            : base(dependencies)
+        {
+        }
+
+        protected HalfsyncWeb(
+            object locker)
+            : base(locker)
+        {
+        }
+
+        protected HalfsyncWeb(
+            ICollection<Dependency> dependencies,
+            object locker)
+            : base(dependencies, locker)
+        {
+        }
+
+        public virtual bool Unregister<T>(
+            string name = null)
+        {
+            var deps = this.dependencies;
+            var unregistered = falsity;
+            if (deps == null)
+            {
+                return unregistered;
+            }
+
+            lock (this.locker)
+            {
+                foreach (var d in deps)
+                {
+                    if (this.tryGet(
+                            d.Content,
+                            d.Name,
+                            name,
+                            out T _))
+                    {
+                        unregistered = deps.Remove(d);
+                        break;
+                    }
+                }
+            }
+
+            return unregistered;
+        }
+    }
+}
