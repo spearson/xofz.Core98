249ed0c59d1a1bf27d473c0aa7d24f5b98dbb8d6
Sam Pearson <xofz@outlook.com>
2024-07-06 18:30:00 -0700

    Add Runner

---
+++ xofz.Core98/Framework/Runner.cs
@@ -0,0 +1,32 @@
+﻿namespace xofz.Framework
+{
+    public class Runner<T>
+    {
+        public virtual T O { get; set; }
+
+        public virtual T Run(
+            Do<T> engine)
+        {
+            var o = this.O;
+            engine?.Invoke(o);
+
+            return o;
+        }
+    }
+
+    public class Runner 
+        : Runner<object>
+    {
+        public virtual T Run<T>(
+            Do<T> engine)
+        {
+            if (this.O is T o)
+            {
+                engine?.Invoke(o);
+                return o;
+            }
+
+            return default;
+        }
+    }
+}
