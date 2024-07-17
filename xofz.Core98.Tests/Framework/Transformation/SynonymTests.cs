c59b467c2546375f5b9f0d7e282bd4890c2c1541
Sam Pearson <xofz@outlook.com>
2024-07-17 01:02:06 -0700

    Add Synonym + tests

---
+++ xofz.Core98.Tests/Framework/Transformation/SynonymTests.cs
@@ -0,0 +1,89 @@
+﻿namespace xofz.Tests.Framework.Transformation
+{
+    using Ploeh.AutoFixture;
+    using xofz.Framework.Transformation;
+    using Xunit;
+
+    public class SynonymTests
+    {
+        public class Context
+        {
+            protected Context()
+            {
+                this.synonym = new Synonym();
+                this.fixture = new Fixture();
+            }
+
+            protected readonly Synonym synonym;
+            protected readonly Fixture fixture;
+        }
+
+        public class When_Nominate_is_called : Context
+        {
+            [Fact]
+            public void Works()
+            {
+                var testLong = this.fixture.Create<long>();
+                var testString = this.fixture.Create<string>();
+                var testInt = this.fixture.Create<int>();
+                var testOb = new object();
+                var source = new[]
+                {
+                    testLong,
+                    testString,
+                    testInt,
+                    testOb
+                };
+
+                Gen<object, int> factory = o =>
+                {
+                    switch (o)
+                    {
+                        case long l:
+                            return (int)l;
+                        case string aString:
+                            return aString.Length;
+                        case int i:
+                            return i;
+                        case object ob:
+                            return ob.GetHashCode();
+                    }
+
+                    return default;
+                };
+
+                byte indexer = 0xFF;
+                const byte zero = 0, one = 1, two = 2, three = 3;
+                foreach (var n in this.synonym.Nominate(
+                             source,
+                             factory))
+                {
+                    ++indexer;
+                    switch (indexer)
+                    {
+                        case zero:
+                            Assert.Equal(
+                                (int)testLong,
+                                n);
+                            break;
+                        case one:
+                            Assert.Equal(
+                                testString.Length,
+                                n);
+                            break;
+                        case two:
+                            Assert.Equal(
+                                testInt,
+                                n);
+                            break;
+                        case three:
+                            Assert.Equal(
+                                testOb.GetHashCode(),
+                                n);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ xofz.Core98/Framework/Transformation/Synonym.cs
@@ -0,0 +1,22 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+
+    public class Synonym
+    {
+        public virtual IEnumerable<K> Nominate<T, K>(
+            IEnumerable<T> source,
+            Gen<T, K> kFactory)
+        {
+            if (source == null || kFactory == null)
+            {
+                yield break;
+            }
+
+            foreach (var item in source)
+            {
+                yield return kFactory(item);
+            }
+        }
+    }
+}
