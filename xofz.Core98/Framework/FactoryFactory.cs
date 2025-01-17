8bdb945e5eb1fb42772ff1f52d1d1c9a25d4f9b9
Sam Pearson <xofz@outlook.com>
2025-01-17 15:28:21 -0800

    Add FactoryFactory

---
+++ xofz.Core98/Framework/FactoryFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace xofz.Framework
+{
+    public class FactoryFactory
+    {
+        public virtual Factory Create(
+            Factory factory)
+        {
+            if (factory == null)
+            {
+                return default;
+            }
+
+            return factory.Create<Factory>();
+        }
+    }
+}
